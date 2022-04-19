e1c6c8b65b62b39c34d18f2c0d785e9f950ef502
Garrett Peake <gepeake@gmail.com>
2022-04-19 14:59:54 -0700

    Trying to get test script working

--- src/entry.ts
+++ src/entry.ts
@@ -1,10 +1,10 @@
 import { DiscordAuthController } from "./authcontrollers/discordauthcontroller";
 export { DiscordAuthController } from "./authcontrollers/discordauthcontroller";
 
-import { Router, handleFetch, handleScheduled } from "pioche";
+import { Router, DefaultHandlers } from "pioche";
 import { easyPreflight } from "./middleware";
 
 Router.useBefore(easyPreflight);
 // Router.register(DiscordAuthController, {binding: "DISCORDAUTH"});
 
-export default{handleFetch, handleScheduled};
+export default DefaultHandlers;
